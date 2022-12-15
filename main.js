7a7576a039879e4566ab5fd1b4596cfe6ab20e43
writecards <rw2423@nyu.edu>
2022-12-15 11:42:21 -0500

    style

--- main.js
+++ main.js
@@ -11,13 +11,16 @@ async function fetchDataFromAPIEndpoint(){
     <article class="card">
     <a href = "${card.properties.Link.url}">
 <img
+class="card__image"
+style = "width:200px"
   src="${card.properties.Image.files[0].external.url}"
   alt="${card.properties.Name.title[0].plain_text}"
-  class="card__image"
+  
 />
 <h2 class="card__heading">${card.properties.Name.title[0].plain_text}</h2>
+<br>
 <div class="card__content">
-  <p id = "card__content__text">
+  <p id = "card__content__text" style = "font-size: 1.5vh">
   ${card.properties.Content.rich_text[0].plain_text}
   </p>
 </div>
